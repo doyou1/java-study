263642e8537b5216e259223a51d1eaab6c56a594
doyou1 <tla149@daum.net>
2021-01-12 00:40:01 +0900

    Java8 FunctionType에 대하여

--- java8-practice/src/main/java/FunctionalInterfaceExamples.java
+++ java8-practice/src/main/java/FunctionalInterfaceExamples.java
@@ -3,7 +3,15 @@ import java.util.function.Function;
 public class FunctionalInterfaceExamples {
 
     public static void main(String[] args) {
-        Function
+        // 람다식 표현
+        Function<String,Integer> toInt = (value) -> Integer.parseInt(value);
+
+        final Integer number = toInt.apply("100");
+        System.out.println(number);
+
+        final Function<Integer, Integer> identity = Function.identity();
+
+        System.out.println(identity.apply(000));
     }
 
 }
